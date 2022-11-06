8e85ededbd9e59c0d05a94b8f0cd4a708b2b0e7d
nishantt19 <nyd44533@gmail.com>
2022-11-06 22:15:27 +0530

    second commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import Heading from "./Heading"
 import Note from "./Note"
 import Footer from "./Footer";
+import notes from "../notes.js"
 
+
+const createNote=(note)=>{
+    return (
+        <Note title ={note.title} content={note.content} />
+    )
+}
 
 function App(){
     return(
         <div>
             <Heading />
-            <Note />
+           
+            {notes.map(createNote)}
             <Footer />
         </div>
     )
